576ff86c2b8dd0e283303142bd931c049398fc38
Kevin Breid <kevinbreid@yahoo.com>
2023-10-13 17:45:44 +0200

    Working on OOP design in Python

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, annas):
+        word = self.word
+        result = []
+
+        for entry in annas:
+            if sorted([letter for letter in word]) == sorted([letter for letter in entry]):
+                result.append(entry) 
+        return result
+            
+
